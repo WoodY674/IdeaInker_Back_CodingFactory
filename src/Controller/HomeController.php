3e416664622794f9604651546be3754ba3778200
antoine <antoine-haller@outlook.fr>
2021-11-05 16:53:24 +0100

    Ajout d'un modal et d'un formulaire, envoi en bdd du mail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\FeedbackUserData;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +13,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/download',name: 'download')]
+    public function download(EntityManagerInterface $entityManager)
+    {
+        $feedbackData = new FeedbackUserData();
+        $feedbackData->setEmail($_POST['email']);
+        $entityManager->persist($feedbackData);
+        $entityManager->flush();
         return $this->render('home/index.html.twig');
     }
 }
