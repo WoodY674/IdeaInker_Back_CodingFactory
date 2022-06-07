c861e4f963fba9b7b1aa8f9b922355ff46e8ebf1
Lexo783 <florentdebuchy@orange.fr>
2022-06-07 15:28:00 +0200

    fix file import

--- src/Form/FeedBackType.php
+++ src/Form/FeedBackType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use App\Entity\FeedbackUserData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +18,6 @@ class FeedBackType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => FeedbackUserData::class,
         ]);
     }
 }
